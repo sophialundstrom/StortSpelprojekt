0d3d723d41cbf35728b525bcfb8661bfe67d25a6
fredrikWes <62289768+fredrikWes@users.noreply.github.com>
2021-09-22 16:15:11 +0200

    jomp

--- StortSpelprojekt/Project/Player.h
+++ StortSpelprojekt/Project/Player.h
@@ -64,9 +64,9 @@ private:
 	float gravity = 9.82f;
 	float timePassed = 0;
 
+	bool jumping = false;
+	bool maxJumpHeight = false;
 
-
-	float maxJumpHeight = 1;
 	float jumpVelocity = 0;
 	float playerVelocity = 0;
 	float airTime = 0;
@@ -88,8 +88,6 @@ private:
 public:
 	void Update(Camera* camera)
 	{
-
-
 		//Rotate camera by cursor movement 
 		camera->Rotate(Event::ReadRawDelta().x * mouseSensitivity, Event::ReadRawDelta().y * mouseSensitivity);
 		Event::ClearRawDelta();
@@ -110,23 +108,9 @@ public:
 			moveDirection += Vector3(1, 0, 0);
 		moveDirection.Normalize();
 
-
 		//Calculate playerJumpVelocity
-		jumpVelocity = sqrtf(2 * gravity * maxJumpHeight);
-
-		if (Event::KeyIsPressed(VK_SPACE))
-		{
-			airTime += Time::GetDelta();
-			playerVelocity = 0.5f * (-gravity) * powf(airTime, 2) + float(jumpVelocity) * airTime;
-		}
-
-		if (Event::KeyIsPressed(VK_BACK))
-		{
-			airTime = 0;
-		}
-
-		std::cout << playerVelocity << std::endl;
-
+		jumpVelocity = sqrtf(2 * gravity * 1);
+		
 
 		//Calculate the radians between the cameras yAxis direction and {0, 0, 1}-Vector.
 		//Aligns the keyboardinputs by the camera direction afterwards via the radian.
@@ -148,6 +132,42 @@ public:
 		moveDirection = moveDirection * (playerMoveSpeed * Time::GetDelta());
 		Vector3 newPlayerPos = position + moveDirection + Vector3(0, playerVelocity, 0);
 
+		if (Event::KeyIsPressed(VK_SPACE))
+		{
+			jumping = true;
+		}
+
+		if (jumping && !Event::KeyIsPressed(VK_SPACE))
+		{
+			if (!maxJumpHeight)
+			{
+				airTime += Time::GetDelta() * 0.005f;
+				playerVelocity = 0.5f * (+gravity) * powf(airTime, 2) + float(jumpVelocity) * airTime;
+				if (newPlayerPos.y >= 20)
+				{
+					maxJumpHeight = true;
+					jumping = false;
+				}
+			}			
+		}
+
+		if (!jumping)
+		{
+			if (playerVelocity >= 0)
+			{
+				airTime += Time::GetDelta() * 0.5f;
+				playerVelocity = 0.5f * (-gravity) * powf(airTime, 2) - float(jumpVelocity) * airTime;
+			}
+			
+			if (newPlayerPos.y <= 5)
+			{
+				airTime = 0;
+				maxJumpHeight = false;
+				jumping = false;
+			}
+			
+		}
+
 		if (newPlayerPos.y < heightMapGroundLevel)
 		{
 			newPlayerPos = Vector3(newPlayerPos.x, heightMapGroundLevel, newPlayerPos.z);
@@ -157,21 +177,17 @@ public:
 		position = newPlayerPos;
 		camera->SetPosition(newCameraPos);
 
-		
 
-
+		std::cout << jumping << std::endl;
 	};
 
 
 	Player()
 		:Model("PlayerArrow")
 	{
-
 		//Player
 		position = { 0, 5, 0 };
 		rotation = { 0, PI, 0 };
-		
-
 	}
 
 	Inventory& Inventory() { return inventory; }
