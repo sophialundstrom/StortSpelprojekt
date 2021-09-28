a97ab9ecb01b4256c78e4d8ca59441204350474b
sophialundstrom <sophialundstrom@hotmail.com>
2021-09-28 19:05:47 +0200

    Texture Blending (3 Textures)

--- StortSpelprojekt/Project/DeferredTerrainPixelShader.hlsl
+++ StortSpelprojekt/Project/DeferredTerrainPixelShader.hlsl
@@ -1,5 +1,7 @@
 // Array of textures since we are also blending based on the height value of the displacement
-Texture2D diffuseTexture : register(t0);
+Texture2D blendTexture : register(t0);
+
+Texture2D textures[3] : register(t1);
 
 sampler wrapSampler : register(s0);
 
@@ -18,7 +20,15 @@ PS_OUTPUT main(PS_INPUT input)
 {
     PS_OUTPUT output;
 
-    output.diffuseTexture = diffuseTexture.Sample(wrapSampler, input.texCoords);
+    const float4 blendValue = blendTexture.Sample(wrapSampler, input.texCoords);
+    
+    const float2 newTex = input.texCoords * 30.0f;
+    
+    const float4 t1 = textures[0].Sample(wrapSampler, newTex) * blendValue.x;
+    const float4 t2 = textures[1].Sample(wrapSampler, newTex) * blendValue.y;
+    const float4 t3 = textures[2].Sample(wrapSampler, newTex) * blendValue.z;
+    
+    output.diffuseTexture = t1 + t2 + t3;
 
 	return output;
 }
