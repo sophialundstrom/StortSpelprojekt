69def6a16a17a0428832dcc27e117423a70af61a
PhilipBerglund <philipberglund1@hotmail.com>
2021-10-08 09:00:33 +0200

    testing

--- StortSpelprojekt/Project/IDRenderer.cpp
+++ StortSpelprojekt/Project/IDRenderer.cpp
@@ -22,28 +22,28 @@ int IDRenderer::GetObjectID(int xPix, int yPix)
 	UINT32* data = static_cast<UINT32*>(mappedResource.pData);
 
 
-	UINT32* imageData = new UINT32[textureDesc.Width * textureDesc.Height * 3];
-	for (int w = 0; w < textureDesc.Width; w++)
-	{
-		for (int h = 0; h < textureDesc.Height; h++)
-		{
-			int currentPixel = (h * textureDesc.Width + w) * 3;
-			imageData[currentPixel] = *data * 255;
-			imageData[currentPixel + 1] = 0;
-			imageData[currentPixel + 2] = 0;
-			data += 1;
-		}
-	}
+	//UINT32* imageData = new UINT32[textureDesc.Width * textureDesc.Height * 3];
+	//for (int w = 0; w < textureDesc.Width; w++)
+	//{
+	//	for (int h = 0; h < textureDesc.Height; h++)
+	//	{
+	//		int currentPixel = (h * textureDesc.Width + w) * 3;
+	//		imageData[currentPixel] = *data * 255;
+	//		imageData[currentPixel + 1] = 0;
+	//		imageData[currentPixel + 2] = 0;
+	//		data += 1;
+	//	}
+	//}
 
-	stbi_write_png("objectID.png", textureDesc.Width, textureDesc.Height, 3, imageData, textureDesc.Width * 3);
-	delete[] imageData;
+	//stbi_write_png("objectID.png", textureDesc.Width, textureDesc.Height, 3, imageData, textureDesc.Width * 3);
+	//delete[] imageData;
 
-	Print(textureDesc.Width);
-	Print(xPix);
-	Print(yPix);
+	//Print(textureDesc.Width);
+	//Print(xPix);
+	//Print(yPix);
 
-	//data += yPix * textureDesc.Width + xPix;
-	//memcpy(&id, data, sizeof(UINT32));
+	data += yPix * textureDesc.Width + xPix;
+	memcpy(&id, data, sizeof(UINT32));
 
 	Graphics::Inst().GetContext().Unmap(idTextureData, 0);
 	return id;
