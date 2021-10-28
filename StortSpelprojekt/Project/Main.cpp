3e4c4c2fb19d3735b2091e02cffc05c9d4a9477e
Sophia Lundström <sophialundstrom@hotmail.com>
2021-10-28 17:34:26 +0200

    ????

--- StortSpelprojekt/Project/Main.cpp
+++ StortSpelprojekt/Project/Main.cpp
@@ -7,11 +7,21 @@
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
 #ifdef _DEBUG
-	//DebugApplication app(hInstance);
-//#else
+	DebugApplication* app = new DebugApplication(hInstance);
+#else
 	Application app(hInstance);
 #endif
-	app.Run();
 
-	return 0;
+	int code = app->Run();
+
+#ifdef _DEBUG
+	if (code != 0)
+	{
+		delete app;
+		Application gameApp(hInstance);
+		gameApp.Run();
+	}
+#endif
+
+	return code;
 }
