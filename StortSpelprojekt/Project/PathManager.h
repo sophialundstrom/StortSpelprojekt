de7b1eaf377fcc593a95306ff56b19a836c28020
mrfogle99 <linushellberg@live.se>
2021-11-05 13:21:06 +0100

    added struct Pathrequest

--- StortSpelprojekt/Project/PathManager.h
+++ StortSpelprojekt/Project/PathManager.h
@@ -3,7 +3,15 @@
 
 struct PathRequest
 {
+	Vector3 pathStart;
+	Vector3 pathEnd;
+	Action<Vector3[], bool> callback;
 
+	public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback) {
+		pathStart = _start;
+		pathEnd = _end;
+		callback = _callback;
+	}
 };
 
 class PathManager
