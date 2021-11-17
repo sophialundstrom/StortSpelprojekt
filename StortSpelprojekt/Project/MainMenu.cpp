ba905aed7dd77af1dbefe11c3a09784bde418445
PhilipBerglund <philipberglund1@hotmail.com>
2021-11-17 14:06:33 +0100

    update

--- StortSpelprojekt/Project/MainMenu.cpp
+++ StortSpelprojekt/Project/MainMenu.cpp
@@ -180,7 +180,7 @@ void MainMenu::Initialize()
 	FBXLoader levelLoader("Models");
 
 	GameLoader gameLoader;
-	gameLoader.Load("Main Menu", scene.GetDrawables());
+	gameLoader.Load("Default", scene.GetDrawables());
 
 	for (auto& [name, drawable] : scene.GetDrawables())
 	{
