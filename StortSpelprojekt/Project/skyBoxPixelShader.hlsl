ec3609f06f9927f6660dcadfee470d536cb7cb5e
SwiggityWAVE <SwiggityWAVE@users.noreply.github.com>
2021-11-29 15:17:25 +0100

    RGBBugFix

--- StortSpelprojekt/Project/skyBoxPixelShader.hlsl
+++ StortSpelprojekt/Project/skyBoxPixelShader.hlsl
@@ -3,5 +3,6 @@ SamplerState sam : register(s0);
 
 float4 main(float3 worldPos : Position) : SV_TARGET
 {
-    return tex.Sample(sam, worldPos);
+    //RGB IS INVERTED FOR SOME REASON
+    return float4(tex.Sample(sam, worldPos).bgr, 1);
 }
