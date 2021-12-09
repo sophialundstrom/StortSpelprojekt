0e43543eb06faac942315a68fdee75f28e667048
PhilipBerglund <philipberglund1@hotmail.com>
2021-12-09 14:08:09 +0100

    Update PerformancePixelShader.hlsl

--- StortSpelprojekt/Project/PerformancePixelShader.hlsl
+++ StortSpelprojekt/Project/PerformancePixelShader.hlsl
@@ -6,5 +6,7 @@ struct PS_INPUT
 float4 main(PS_INPUT input) : SV_TARGET
 {
     float4 finalColor = (1.0f, 1.0f, 1.0f, 1.0f);
+    float4 OK = (0.0f, 0.0f, 1.0f, 1.0f);
+    float4 NOT = (1.0f, 0.0f, 0.0f, 1.0f);
     return finalColor;
 }
